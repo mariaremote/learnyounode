b70f8f663c2a11571a56818e759876b0078ae8c4
Maria <123465523+mariaremote@users.noreply.github.com>
2024-06-25 08:31:31 +0200

    finishes exercise

--- time-server.js
+++ time-server.js
@@ -1,1 +1,30 @@
-``;
+const net = require("net");
+const strftime = require("strftime");
+const port = process.argv[2];
+const serverConfig = { host: "localhost", port: port || 8080 };
+const server = net.createServer();
+
+const format = "%F %H:%M";
+let today = new Date();
+const dateString = strftime(format, today) + "\n";
+
+server.on("connection", (socket) => {
+  // console.log("S: Client connected");
+  // socket.write("S: Welcome to this Server!");
+  socket.write(dateString);
+  socket.end();
+});
+server.on("data", (data) => {
+  console.log(data.toString());
+});
+server.on("end", () => {
+  console.log("S: Server closed connection");
+});
+
+server.listen(serverConfig, () => {
+  console.log(
+    `Server is listening on ${serverConfig.host}:${serverConfig.port}`
+  );
+});
+
+module.exports = { serverConfig };
