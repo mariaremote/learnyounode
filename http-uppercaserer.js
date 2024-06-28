561cae75414cd7047052e150c1236603e1283171
Maria <123465523+mariaremote@users.noreply.github.com>
2024-06-28 21:39:59 +0200

    refactor

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -2,19 +2,13 @@ const http = require("http");
 const port = process.argv[2];
 const map = require("through2-map");
 
-// function upperCase(chunk) {
-//   return chunk.toString().toUpperCase();
-// }
+function upperCase(chunk) {
+  return chunk.toString().toUpperCase();
+}
 
 const server = http.createServer((req, res) => {
   if (req.method === "POST") {
-    req
-      .pipe(
-        map((chunk) => {
-          return chunk.toString().toUpperCase();
-        })
-      )
-      .pipe(res);
+    req.pipe(map(upperCase)).pipe(res);
   }
 });
 
