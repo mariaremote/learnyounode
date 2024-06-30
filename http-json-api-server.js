8ce959e5b631eed6d0802271723b8c23cae70b10
Maria <123465523+mariaremote@users.noreply.github.com>
2024-06-30 15:56:04 +0200

    refactors a little

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,29 +1,30 @@
 const http = require("http");
-const port = process.argv[2];
+const port = process.argv[2] || 8080;
 
 const server = http.createServer((req, res) => {
-  const method = req.method;
   const url = new URL(req.url, `http://${req.headers.host}`);
   const isoFormat = url.searchParams.get("iso");
   // const date = new Date("2024-06-30T03:20:20.670Z");
   const dateTime = new Date(isoFormat);
 
-  // hour-minute-second route
-  if (method === "GET" && url.pathname === "/api/parsetime") {
-    const hours = dateTime.getHours();
-    const minutes = dateTime.getMinutes();
-    const seconds = dateTime.getSeconds();
-    let hhmmss = { hour: hours, minute: minutes, second: seconds };
+  // parsetime route
+  if (req.method === "GET" && url.pathname === "/api/parsetime") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(hhmmss));
+    return res.end(
+      JSON.stringify({
+        hour: dateTime.getHours(),
+        minute: dateTime.getMinutes(),
+        second: dateTime.getSeconds(),
+      })
+    );
   }
-
   // unixtime route
-  if (method === "GET" && url.pathname === "/api/unixtime") {
-    let unixtime = { unixtime: dateTime.getTime() };
+  if (req.method === "GET" && url.pathname === "/api/unixtime") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(unixtime));
+    return res.end(JSON.stringify({ unixtime: dateTime.getTime() }));
   }
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end(`${url.pathname} not found`);
 });
 
 server.listen(port, () => {
